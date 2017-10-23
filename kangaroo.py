33afb2c1b6b4fcd77029b181b276b82a6a8f6431
Diego Milan <diego.088@gmail.com>
2017-10-23 21:03:55 +0100

    Adds kangaroo

---
+++ kangaroo.py
@@ -0,0 +1,29 @@
+#!/bin/python
+import sys
+
+def kangaroo(x1, v1, x2, v2):
+	"""There are two kangaroos on a number line ready to jump in the positive direction 
+	(i.e, toward positive infinity). The first kangaroo starts at location  and moves at 
+	a rate of  meters per jump. The second kangaroo starts at location  and moves at a 
+	rate of  meters per jump. Given the starting locations and movement rates for each kangaroo, 
+	can you determine if they'll ever land at the same location at the same time?"""
+	
+    if x1 == x2:
+    	print "YES"
+    
+    gap = x1 - x2
+    gap_2 = x1 + v1 - x2 - v2
+    gap_diff = abs(gap_2) - abs(gap)
+
+    if gap_diff >= 0:
+    	return "NO"
+
+    if gap % gap_diff == 0:
+        return "YES"
+
+    return "NO"
+
+x1, v1, x2, v2 = raw_input().strip().split(' ')
+x1, v1, x2, v2 = [int(x1), int(v1), int(x2), int(v2)]
+result = kangaroo(x1, v1, x2, v2)
+print(result)
